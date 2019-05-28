0bc448a348226e393c479a88f84e1d5c95825aa5
Rafael Špiljak <rafael.spiljak@fer.hr>
2019-05-28 10:46:06 +0200

    SpeedOfMe Download

--- app/src/main/java/hr/fer/rafael/speedtest/MainActivity.java
+++ app/src/main/java/hr/fer/rafael/speedtest/MainActivity.java
@@ -1,34 +1,48 @@
 package hr.fer.rafael.speedtest;
 
+import android.content.SyncStatusObserver;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.PrintStream;
+import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.net.SocketAddress;
+import java.net.UnknownHostException;
 import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Scanner;
 
 public class MainActivity extends AppCompatActivity {
-
+    FileInputStream fis;
+    InputStream is;
     Button startButton;
     TextView pingView;
     TextView downloadView;
     TextView uploadView;
     TextView centralView;
+    Spinner spinner;
     int i=0,n=10240,h=0,countzeros=0;
     byte[] buff=new byte[n];
     byte[] abuf=new byte[n];
@@ -50,6 +64,10 @@ public class MainActivity extends AppCompatActivity {
         downloadView=(TextView) findViewById(R.id.download);
         uploadView=(TextView) findViewById(R.id.upload);
         centralView=(TextView) findViewById(R.id.centralView);
+        spinner=(Spinner) findViewById(R.id.spinner);
+        ArrayAdapter<String> spinnerList=new ArrayAdapter<String>(MainActivity.this,android.R.layout.simple_expandable_list_item_1,getResources().getStringArray(R.array.methods));
+        spinnerList.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(spinnerList);
 
     }
 
@@ -58,12 +76,20 @@ public class MainActivity extends AppCompatActivity {
         startButton.setEnabled(false);
         startButton.setText("Measuring");
         pingView.setText("0 ms");
-        downloadView.setText("0 Mb/s");
-        uploadView.setText("0 Mb/s");
+        downloadView.setText("0 Mbps");
+        uploadView.setText("0 Mbps");
         centralView.setText(" ");
         String number,temp;
+        spinner.getSelectedItem().toString();
+
+        try{
+            is=getResources().openRawResource(R.raw.file);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
         final String ipAddress="46.101.104.253";
-        final int port=2222;
+        final int port=8080;
         new Thread(new Runnable() {
             @Override
             public void run() {
@@ -74,81 +100,139 @@ public class MainActivity extends AppCompatActivity {
                     }
                 });
                 try{
-                    Socket s=new Socket("46.101.104.253",5533);
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            Toast.makeText(getApplicationContext(),"Server connection established",Toast.LENGTH_SHORT).show();
+
+                    int method=0;
+                    String methodChosen=spinner.getSelectedItem().toString();
+                    if(methodChosen.equals("Ookla")){
+                        method=0;
+                    }else if(methodChosen.equals("SpeedOfMe")){
+                        method=1;
+                    }else if(methodChosen.equals("SaveDataMethod")){
+                        method=2;
+                    }
+                    final DownloadTest download=new DownloadTest(method);
+                    final PingTest ping=new PingTest(ipAddress);
+                    final UploadTest upload=new UploadTest(is,method);
+                    boolean pingStarted=false;
+                    boolean downloadStarted=false;
+                    boolean uploadStarted=false;
+                    boolean pingFinished=false;
+                    boolean downloadFinished=false;
+                    boolean uploadFinished=false;
+
+                    while(true){
+                        if(!pingStarted&&!downloadStarted&&!uploadStarted){
+                            ping.start();
+                            pingStarted=true;
                         }
-                    });
-                    DataInputStream input=new DataInputStream(s.getInputStream());
-                    int num=100;
-                    PrintStream p=new PrintStream(s.getOutputStream());
-                    InetAddress inet=InetAddress.getByName(ipAddress);
-                    final float pingavg=ping(inet,num);
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            pingView.setText(String.format("%.1f",pingavg)+" ms");
+                        if(pingFinished&&!downloadStarted&!uploadStarted){
+                            download.start();
+                            downloadStarted=true;
                         }
-                    });
-                    h=input.read(abuf,0,n);
-                    start=System.currentTimeMillis();
-                    while(System.currentTimeMillis()-start<5000){
-                        d=System.currentTimeMillis();
-                        h=input.read(buff,0,n);
-                        cost=System.currentTimeMillis()-d;
-                        i+=h;
-                        final double momentSpeed=0.008*h/cost;
-                        if(i==-1&&i>=20480000) break;
-                        if(cost!=0){
-                            if(Double.isInfinite(momentSpeed)){
-                            }else if(Double.isNaN(momentSpeed)&&(momentSpeed)<=10){
-                                continue;
-                            }else {
-                                download.add(momentSpeed);
-                                if((System.currentTimeMillis()-start)%100==0){
+                        if(pingFinished&&downloadFinished&&!uploadStarted){
+                            //upload.start();
+                            uploadStarted=true;
+                        }
+
+                        /*pingtest*/
+                        if (pingStarted) {
+                            if(ping.averagePing==0.0){
+                                runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        //Toast.makeText(getApplicationContext(),"Server is not available",Toast.LENGTH_SHORT);
+
+                                    }
+                                });
+                            }else{
+                                runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        pingView.setText(String.format("%.0f",ping.getAveragePing())+" ms");
+                                    }
+                                });
+                            }
+                        }else{
+                            centralView.setText(String.format("%.0f",ping.getInstantPing())+" ms");
+                        }
+                        /*downloadtest*/
+                        if(pingFinished&&!uploadFinished){
+                            System.out.println(download.finalDownloadRate);
+                            if (downloadFinished&&!uploadFinished){
+                                if (download.getFinalDownloadRate()==0.0){
+                                    System.out.println("Error while downloading");
+                                }else{
                                     runOnUiThread(new Runnable() {
                                         @Override
                                         public void run() {
-                                            if(cost!=0&&(momentSpeed)!=0&&!Double.isInfinite(momentSpeed)) {
-                                                centralView.setText(String.format("%.1f", momentSpeed));
-                                            }
+                                            downloadView.setText(String.format("%.2f",download.getFinalDownloadRate())+" Mbps");
                                         }
                                     });
                                 }
+                            }else{
+                                runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        centralView.setText(String.format("%.2f",download.getInstantDownloadRate())+" Mbps");
+                                    }
+                                });
+                            }
+
+                        }
+
+                        if(pingFinished&&downloadFinished){
+                            if(uploadFinished){
+                                if(upload.getFinalUploadRate()==0){
+                                    System.out.println("Upload error");
+                                }else{
+                                    runOnUiThread(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            uploadView.setText(String.format("%.2f",upload.getFinalUploadRate())+" Mbps");
+                                        }
+                                    });
+                                }
+                                }else{
+                                runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        centralView.setText(String.format("%.2f",upload.getInstantUploadRate())+" Mbps");
+                                    }
+                                });
+                            }
+                            }
+
+                        if(pingFinished&&downloadFinished){
+                            break;
+                        }
+                        if(ping.isFinished()&&!downloadFinished&&!uploadFinished){
+
+
+
+                                pingFinished=true;
+
+                        }
+                        if(download.isFinished()&&pingFinished&&!uploadFinished){
+
+                            try{
+                                Thread.sleep(2000);
+                                downloadFinished=true;
+                            }catch (Exception e){
+
                             }
                         }
+                        /*if(upload.isFinished()&&pingFinished&&downloadFinished){
 
-                    }
-                    download=speedMethod(download);
+                            try{
+                                Thread.sleep(4000);
+                                uploadFinished=true;
+                            }catch(Exception e){
 
-                    for(double d:download){
-                        if(d==0)countzeros++;
-                        avg+=d;
+                            }
+                        }*/
                     }
-                    avg=avg/(download.size()-countzeros);
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            centralView.setText(" ");
-                            downloadView.setText(String.format("%.1f Mb/s",avg));
-                        }
-                    });
-                    s.close();
-                    Socket s1=new Socket("46.101.104.253",5534);
-                    DataOutputStream dis=new DataOutputStream(new BufferedOutputStream(s1.getOutputStream()));
-                    upload(dis,abuf);
-                    dis.flush();
-                    Socket s2=new Socket("46.101.104.253",5535);
-                    Scanner sc=new Scanner(s2.getInputStream());
-                    final String uploadSpeed=sc.nextLine();
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            uploadView.setText(uploadSpeed+" Mb/s");
-                        }
-                    });
+
+
                 }catch(Exception e){
                     e.printStackTrace();
                 }
@@ -169,60 +253,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    static float ping(InetAddress inet,int number) throws IOException {
-        long a;
-        long cost;
-        float avgping=0;
-        ArrayList<Long> times=new ArrayList<>();
-        long d=System.currentTimeMillis();
-        while(System.currentTimeMillis()-d<1000){
-            a=System.currentTimeMillis();
-            if(inet.isReachable(5000)) {
-                cost=System.currentTimeMillis()-a;
-                times.add(cost);
-                avgping+=cost;
-            }else {
 
-            }
-        }
 
-        float pingavg=avgping/times.size();
-        return pingavg;
-    }
 
-    static double download(DataInputStream dis,boolean method) throws IOException {
-        int i=0;
-        int n=10240;
-        byte[] buff=new byte[n];
-        long start,cost;
-        ArrayList<Double> download=new ArrayList<>();
-        while(n!=-1) {
-            start=System.currentTimeMillis();
-            if((n=dis.read(buff, 0, n))==-1) {
-                break;
-            }else {
 
-                cost=System.currentTimeMillis()-start;
-                if(cost!=0) {
-                    download.add(0.008*n/cost);
-                }
-                i+=n;
-
-            }
-        }
-        Log.i("  ",download.toString());
-        if(method==true) {
-            download=speedMethod(download);
-        }
-        double avg=0;
-        for(double d:download) {
-            avg+=d;
-        }
-        avg=avg/download.size();
-        System.out.println(i);
-        return avg;
-
-    }
 
     static ArrayList<Double> speedMethod(ArrayList<Double> download){
         Collections.sort(download);
@@ -238,17 +272,8 @@ public class MainActivity extends AppCompatActivity {
         return download;
 
     }
-    static void upload(DataOutputStream out, byte[] abuf) throws IOException {
-        long start=System.currentTimeMillis();
-        while(System.currentTimeMillis()-start<5000) {
-            try{
-                out.write(abuf,0,10240);
-                out.flush();
-                out.flush();
-            }catch(Exception e){
 
-            }
 
-        }
-    }
 }
+
+
